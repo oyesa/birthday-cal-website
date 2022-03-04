56bdc50d05683840d2def07c098fb00b7e630172
oyesa <mercyoyesa2018@gmail.com>
2022-03-04 16:33:52 +0300

    add span details

---
+++ index.js
@@ -0,0 +1,14 @@
+var yearError = document.getElementById('year-error')
+var yearError = document.getElementById('year-error')
+var yearError = document.getElementById('year-error')
+var yearError = document.getElementById('year-error')
+
+
+function validateYear({
+  var name = document.getElementById('birth-year').value;
+
+  if(year.length == 0){
+    yearError.innerHTML = 'Year is required';
+    return false;
+  }
+})
