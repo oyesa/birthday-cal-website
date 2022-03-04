96b8cbcade6f919aa44cad7eee7a40c68fa7c8c8
oyesa <mercyoyesa2018@gmail.com>
2022-03-04 16:45:00 +0300

    update index.js sheet

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var yearError = document.getElementById('year-error')
-var yearError = document.getElementById('year-error')
-var yearError = document.getElementById('year-error')
-var yearError = document.getElementById('year-error')
+var monthError = document.getElementById('month-error')
+var dayError = document.getElementById('day-error')
+var genderError = document.getElementById('gender-error')
+var submitError = document.getElementById('submit-error')
 
 
 function validateYear({
