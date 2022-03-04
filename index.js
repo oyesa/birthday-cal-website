15575d1c6c9e9e815c20998a2fcdf7cb2b5cca31
oyesa <mercyoyesa2018@gmail.com>
2022-03-04 16:51:16 +0300

    add function to validate year

--- index.js
+++ index.js
@@ -5,11 +5,25 @@ var genderError = document.getElementById('gender-error')
 var submitError = document.getElementById('submit-error')
 
 
-function validateYear({
-  var name = document.getElementById('birth-year').value;
+function validateYear(year) {
+  var text = /^[0-9]+$/;
+  if (year != 0) {
+      if ((year != "") && (!text.test(year))) {
 
-  if(year.length == 0){
-    yearError.innerHTML = 'Year is required';
-    return false;
+          alert("Please Enter Numeric Values Only");
+          return false;
+      }
+
+      if (year.length != 4) {
+          alert("Year is not proper. Please check");
+          return false;
+      }
+      var current_year=new Date().getFullYear();
+      if((year < 1920) || (year > current_year))
+          {
+          alert("Year should be in range 1920 to current year");
+          return false;
+          }
+      return true;
   }
-})
+}
